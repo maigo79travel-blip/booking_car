a70351034342e27370cff2be8479c88860479921
Dai Do <doanhdaigr5.2004@gmail.com>
2026-08-23 22:05:41 +0700

    fix(cms): update tailwind switch classes and clear stale client directory

--- src/components/admin/ContentManager.tsx
+++ src/components/admin/ContentManager.tsx
@@ -1331,7 +1331,7 @@ export default function ContentManager({
                   }
                   className="sr-only peer"
                 />
-                <div className="w-11 h-6 bg-slate-300 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-slate-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+                <div className="w-11 h-6 bg-slate-300 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border-slate-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
               </label>
             </div>
 
